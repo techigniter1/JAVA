f234fe5962c991ed925a38e10f72f66b69012396
Rishabh Yadav <163836298+techigniter1@users.noreply.github.com>
2025-11-03 16:20:27 +0530

    Create Appconfig.java

---
+++ Spring Basics and Hibernate/Part A: Dependency Injection in Spring Using Java-Based Configuration/Appconfig.java
@@ -0,0 +1,18 @@
+package parta;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public Course course() {
+        return new Course("Spring Framework Fundamentals");
+    }
+
+    @Bean
+    public Student student() {
+        return new Student(course());
+    }
+}
